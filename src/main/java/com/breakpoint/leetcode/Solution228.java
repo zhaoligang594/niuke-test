f61be78a1efc561a78fb4cbd98fa77b43925a351
zhaoligang <zhaoligang@kuaishou.com>
2021-01-10 19:37:12 +0800

    228题再次求解

--- src/main/java/com/breakpoint/leetcode/Solution228.java
+++ src/main/java/com/breakpoint/leetcode/Solution228.java
@@ -13,7 +13,7 @@ import java.util.List;
 public class Solution228 {
 
     public static void main(String[] args) {
-        int[] nums = {-1};
+        int[] nums = {0,2,3,4,6,8,9};
         List<String> list = new Solution228().summaryRanges(nums);
         System.out.println(list);
     }
@@ -21,30 +21,53 @@ public class Solution228 {
     public List<String> summaryRanges(int[] nums) {
         if (nums == null || nums.length == 0) return Collections.emptyList();
         List<String> res = new ArrayList<>();
-        StringBuilder sb = new StringBuilder();
-        sb.append(nums[0]);
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i - 1] + 1 == nums[i]) {
-                sb.append("@").append(nums[i]);
-            } else {
-                int index = sb.lastIndexOf("@");
-                int start = sb.indexOf("@");
-                if (index <= 0) {
-                    res.add(sb.toString());
+        int pre = 0, cur = 0;
+        for (; cur < nums.length; cur++) {
+            if (cur > pre && nums[cur - 1] + 1 != nums[cur]) {
+                if (pre != cur - 1) {
+                    res.add(nums[pre] + "->" + nums[cur - 1]);
                 } else {
-                    res.add(sb.substring(0, start) + "->" + sb.substring(index + 1));
+                    res.add(nums[pre] + "");
                 }
-                sb.delete(0, sb.length());
-                sb.append(nums[i]);
+                pre = cur;
             }
         }
-        int index = sb.lastIndexOf("@");
-        int start = sb.indexOf("@");
-        if (index <= 0) {
-            res.add(sb.toString());
+        if (pre != cur - 1) {
+            res.add(nums[pre] + "->" + nums[cur - 1]);
         } else {
-            res.add(sb.substring(0, start) + "->" + sb.substring(index + 1));
+            res.add(nums[pre] + "");
         }
         return res;
     }
+
+
+//    public List<String> summaryRanges(int[] nums) {
+//        if (nums == null || nums.length == 0) return Collections.emptyList();
+//        List<String> res = new ArrayList<>();
+//        StringBuilder sb = new StringBuilder();
+//        sb.append(nums[0]);
+//        for (int i = 1; i < nums.length; i++) {
+//            if (nums[i - 1] + 1 == nums[i]) {
+//                sb.append("@").append(nums[i]);
+//            } else {
+//                int index = sb.lastIndexOf("@");
+//                int start = sb.indexOf("@");
+//                if (index <= 0) {
+//                    res.add(sb.toString());
+//                } else {
+//                    res.add(sb.substring(0, start) + "->" + sb.substring(index + 1));
+//                }
+//                sb.delete(0, sb.length());
+//                sb.append(nums[i]);
+//            }
+//        }
+//        int index = sb.lastIndexOf("@");
+//        int start = sb.indexOf("@");
+//        if (index <= 0) {
+//            res.add(sb.toString());
+//        } else {
+//            res.add(sb.substring(0, start) + "->" + sb.substring(index + 1));
+//        }
+//        return res;
+//    }
 }
